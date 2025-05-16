7a17b36a35c83847ce87913dd83209d3306da023
johnson <owm30911@gmail.com>
2025-05-16 14:05:19 +0800

    init micro-patteern project

---
+++ micro-pattern/src/main/java/org/example/micropattern/MicroPatternApplication.java
@@ -0,0 +1,13 @@
+package org.example.micropattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroPatternApplication.class, args);
+	}
+
+}
---
+++ micro-pattern/src/test/java/org/example/micropattern/MicroPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.micropattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroPatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
